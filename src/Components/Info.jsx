cfe793243de957559bedb4dec77443f7d5ca0e82
Jesse Wachtel <jwachtel96@gmail.com>
2021-12-30 20:16:33 -0500

    12/30

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -16,8 +16,8 @@ export default function Info(props) {
 					</div>
 					<iframe
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3163.5657988631315!2d-77.92141158563865!3d37.54173143330785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b162a482302e43%3A0x28323249736b601c!2sPowhatan%20Courthouse%20Historic%20District!5e0!3m2!1sen!2sus!4v1638493891742!5m2!1sen!2sus"
-						width="275"
-						height="275"
+						width="600"
+						height="400"
 						title="Map to Pizza King"
 					></iframe>
 				</div>
