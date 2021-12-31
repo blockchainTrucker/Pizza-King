bccdbe9cef4d7a8fd7d7830e6ab2d4e4eeb2de8f
Jesse Wachtel <jwachtel96@gmail.com>
2021-12-30 23:49:54 -0500

    12/30

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,9 @@ export default function Footer(props) {
 	return (
 		<div className="container">
 			<footer>
-				Made by{" "}
-				<a href="https://github.com/jwachtel96">
-					Jesse Wachtel using React.
-				</a>
+				{"Made by "}
+				<a href="https://github.com/jwachtel96">Jesse Wachtel</a>
+				{" using React."}
 			</footer>
 		</div>
 	);
